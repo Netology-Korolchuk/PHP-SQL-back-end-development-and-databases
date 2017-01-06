cd13920ae0ab68df9aba319af1b72b218bde06ce
Netology-Korolchuk <post@a4st.ru>
2017-01-06 15:58:03 +0700

    Create notest.php

---
+++ lesson7/errors/notest.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8" lang="ru">
+    <title>Ошибка теста</title>
+</head>
+<body>
+    <h1>Ошибка:</h1>
+    <h2>Не удалось переместить файл теста в директорию на сервере.</h2>
+    <a href="../admin.php">Перейти на страницу загрузки тестов</a>
+</body>
+</html>
