4afc34b835c8198667dc1ca4eb97cff6da9fc160
Netology-Korolchuk <post@a4st.ru>
2017-01-10 15:17:52 +0700

    Add files via upload

---
+++ lesson8/errors/404.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8" lang="ru">
+    <title>Ошибка теста</title>
+</head>
+<body>
+    <h1>Ошибка 404:</h1>
+    <h2>Файл теста не найден.</h2>
+    <a href="../list.php">Перейти на страницу со списком тестов</a>
+</body>
+</html>
