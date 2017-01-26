102d8c86ad2d6cb37190a63aba10358ada93e6ce
Netology-Korolchuk <post@a4st.ru>
2017-01-26 11:39:37 +0700

    Update config.php

--- lesson12/config.php
+++ lesson12/config.php
@@ -1,6 +1,6 @@
 <?php
 define ("DB_HOST", "localhost");
-define ("DB_NAME", "u0209448_netology");
+define ("DB_NAME", "books");
 define ("DB_PORT", "3306");
-define ("DB_USER", "u0209_netology");
-define ("DB_PASS", "EP_1rerV");
+define ("DB_USER", "1111");
+define ("DB_PASS", "1111");
